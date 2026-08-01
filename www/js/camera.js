24fabaed19b1b2c9c7273a289069250f9353260a
siwrad82 <siwrad86@gmail.com>
2026-08-01 20:15:53 +0700

    Improve Camera Preview handling (Build 005.3.1)

--- www/js/camera.js
+++ www/js/camera.js
@@ -6,9 +6,7 @@ Version : 1.0.0
 Build   : 005.2
 ====================================
 */
-let currentPhoto = null;
 
-const btnPreview = document.getElementById("btnPreview");
 const btnCamera = document.getElementById("btnCamera");
 const previewImage = document.getElementById("previewImage");
 const previewText = document.getElementById("previewText");
@@ -16,32 +14,17 @@ const previewText = document.getElementById("previewText");
 btnCamera.addEventListener("click", async () => {
     try {
         const photo = await Capacitor.Plugins.Camera.getPhoto({
-    quality: 90,
-    allowEditing: false,
-    resultType: "uri",
-    source: "CAMERA"
-});
+            quality: 90,
+            allowEditing: false,
+            resultType: "uri",
+            source: "CAMERA"
+        });
 
-currentPhoto = photo.webPath;
-console.log("Photo:", photo);
-console.log("currentPhoto:", currentPhoto);
-
-previewImage.src = currentPhoto;
-previewImage.style.display = "block";
-previewText.style.display = "none";
+        previewImage.src = photo.webPath;
+        previewImage.style.display = "block";
+        previewText.style.display = "none";
 
     } catch (e) {
         console.error("Camera Error:", e);
     }
-});
-btnPreview.addEventListener("click", () => {
-
-    if (currentPhoto) {
-        previewImage.src = currentPhoto;
-        previewImage.style.display = "block";
-        previewText.style.display = "none";
-    } else {
-        alert("Belum ada foto.");
-    }
-
 });
